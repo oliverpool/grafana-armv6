475351741ebb3694e9a1600d267afe00d9498a2c
oliverpool <git@olivier.pfad.fr>
2022-03-27 22:43:09 +0200

    -ccx

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ apt-get -y install golang-go curl
 mkdir grafana
 cd grafana
 curl --location https://github.com/grafana/grafana/archive/refs/tags/v8.4.4.tar.gz | tar -xz --strip-components=1
-LDFLAGS="-static" go run build.go -goarch=armv6 -cgo-enabled=1 -cc=arm-linux-gnueabihf-gcc -cxx=arm-linux-gnueabihf-g++ -pkg-arch=armhf build build-server
+LDFLAGS="-static" go run build.go -goarch=armv6 -cgo-enabled=1 -cc=arm-linux-gnueabihf-gcc -pkg-arch=armhf build build-server
 
 # GOARCH=arm CGO_ENABLED=1 CC=arm-linux-gnueabihf-gcc go build \
 #   -ldflags "-linkmode external -extldflags -static" \

387ad77d449a7ba07d98e4a71b7c1cb8098a47a1
oliverpool <git@olivier.pfad.fr>
2022-03-27 22:22:19 +0200

    arm

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ apt-get -y install golang-go curl
 mkdir grafana
 cd grafana
 curl --location https://github.com/grafana/grafana/archive/refs/tags/v8.4.4.tar.gz | tar -xz --strip-components=1
-GOARCH=arm64 CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc go build \
+GOARCH=arm CGO_ENABLED=1 CC=arm-linux-gnueabihf-gcc go build \
   -ldflags "-linkmode external -extldflags -static" \
   ./pkg/cmd/grafana-server/
 ls -alh grafana-server

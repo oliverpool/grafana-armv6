73a3aaa0c94fad0592727014d70edfc2b6683444
oliverpool <git@olivier.pfad.fr>
2022-03-27 22:59:41 +0200

    sed

--- test.sh
+++ test.sh
@@ -10,6 +10,16 @@ curl --location https://github.com/grafana/grafana/archive/refs/tags/v8.4.4.tar.
 
 # compile
 make gen-go
+cat pkg/build/cmd.go
+sed -i "s/ -extldflags/ -linkmode external -extldflags/w changelog.txt" "pkg/build/cmd.go"
+cat pkg/build/cmd.go
+if [ -s changelog.txt ]; then
+    cat changelog.txt
+else
+    echo "could not add -linkmode external"
+    exit 1
+fi
+
 LDFLAGS="-static" go run build.go -goarch=armv6 -cgo-enabled=1 -cc=arm-linux-gnueabihf-gcc -pkg-arch=armhf build build-server
 
 # GOARCH=arm CGO_ENABLED=1 CC=arm-linux-gnueabihf-gcc go build \

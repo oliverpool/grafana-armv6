919c1c88e0117ddb451137205dff59246118cbe2
oliverpool <git@olivier.pfad.fr>
2022-03-27 22:04:33 +0200

    download sources

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
 set -x
 echo "before"
 apt-get -y install golang-go
+wget -O src.tar.gz https://github.com/grafana/grafana/archive/refs/tags/v8.4.4.tar.gz
+tar xf src.tar.gz
+ls
+cd src
+ls
 GOARCH=arm64 CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc go build \
   -ldflags "-linkmode external -extldflags -static" \
   github.com/grafana/grafana/pkg/cmd/grafana-server@v8.4.4

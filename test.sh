81d34df9f8862b48e981098efb2601aca8f48546
oliverpool <git@olivier.pfad.fr>
2022-03-27 23:11:24 +0200

    -go container

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 # fail on error
 set -euxo pipefail
 
-apt-get -y install golang-go curl
+apt-get -y install curl
 
 # download
 mkdir grafana
@@ -10,9 +10,7 @@ curl --location https://github.com/grafana/grafana/archive/refs/tags/v8.4.4.tar.
 
 # compile
 make gen-go
-cat pkg/build/cmd.go
 sed -i "s/ -extldflags/ -linkmode external -extldflags/w changelog.txt" "pkg/build/cmd.go"
-cat pkg/build/cmd.go
 if [ -s changelog.txt ]; then
     cat changelog.txt
 else
@@ -20,7 +18,7 @@ else
     exit 1
 fi
 
-LDFLAGS="-static" go run build.go -goarch=armv6 -cgo-enabled=1 -cc=arm-linux-gnueabihf-gcc -pkg-arch=armhf build build-server
+LDFLAGS="-static" go run build.go -goarch=armv6 -cgo-enabled=1 -cc=arm-linux-gnueabihf-gcc -pkg-arch=armhf build-server
 
 # GOARCH=arm CGO_ENABLED=1 CC=arm-linux-gnueabihf-gcc go build \
 #   -ldflags "-linkmode external -extldflags -static" \

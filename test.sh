fafe2311ed6d64b4da2117fead8de5c5ca4c06ce
oliverpool <git@olivier.pfad.fr>
2022-03-27 22:18:42 +0200

    build

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 # fail on error
 set -euxo pipefail
 
-echo "before"
 apt-get -y install golang-go curl
+
 mkdir grafana
 cd grafana
 curl --location https://github.com/grafana/grafana/archive/refs/tags/v8.4.4.tar.gz | tar -xz --strip-components=1
-ls
 GOARCH=arm64 CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc go build \
   -ldflags "-linkmode external -extldflags -static" \
-  pkg/cmd/grafana-server
+  ./pkg/cmd/grafana-server/
+ls -alh grafana-server
 echo "after"

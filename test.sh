b7c2e038db74ca8bd36e81cc245653e21c957cbb
oliverpool <git@olivier.pfad.fr>
2022-03-27 21:55:00 +0200

    -y

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 set -x
 echo "before"
-apt-get install golang
+apt-get -y install golang
 GOARCH=arm64 CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc go build \
   -ldflags "-linkmode external -extldflags -static" \
   github.com/grafana/grafana/pkg/cmd/grafana-server
